77a3b2afd3161c396d47b6b4b13fa5f2712033e4
WBuJue <bujue600@gmail.com>
2026-04-21 16:20:44 +0800

    初学Typescript

---
+++ 10_Typescript/demo.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const person = {
+    name: '李四',
+    age: 18
+};
+console.log(`我叫${person.name}，我今年${person.age}岁了`);
+//# sourceMappingURL=demo.js.map
