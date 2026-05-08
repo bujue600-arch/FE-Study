c15a704bf854b2b90b49fafc467767d39e00b9a8
WBuJue <bujue600@gmail.com>
2026-05-08 11:04:17 +0800

    5.8 力扣两题

---
+++ 00_高频算法/04_20. 有效的括号.js
@@ -0,0 +1,29 @@
+https://leetcode.cn/problems/valid-parentheses/description/
+
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function (s) {
+    const stack = []
+    const map = new Map()
+    map.set("(", ")")
+    map.set("[", "]")
+    map.set("{", "}")
+
+    for (let i = 0; i < s.length; i++) {
+        //是左括号就入栈
+        if (map.has(s[i])) {
+            stack.push(s[i])
+            //是右括号就去跟栈尾匹配
+        } else {
+            if (map.get(stack[stack.length - 1]) !== s[i]) {
+                return false
+            } else {
+                stack.pop()
+            }
+        }
+    }
+
+    return stack.length===0
+};
---
+++ 00_高频算法/05_415. 字符串相加.js
@@ -0,0 +1,29 @@
+https://leetcode.cn/problems/add-strings/description/
+
+/**
+ * @param {string} num1
+ * @param {string} num2
+ * @return {string}
+ */
+var addStrings = function (num1, num2) {
+    let i = num1.length - 1;
+    let j = num2.length - 1;
+    let carry = 0;
+    const res = [];
+
+    while (i >= 0 || j >= 0 || carry > 0) {
+        // 使用 i 和 j 分别从 num1 和 num2 取值
+        let n1 = i >= 0 ? parseInt(num1[i]) : 0;
+        let n2 = j >= 0 ? parseInt(num2[j]) : 0; // 修改点：使用 num2[j]
+
+        let sum = n1 + n2 + carry;
+        res.push(sum % 10); // 使用 push 性能更好
+        carry = Math.floor(sum / 10);
+        
+        i--;
+        j--;
+    }
+
+    // 最后反转数组并转为字符串
+    return res.reverse().join("");
+};
